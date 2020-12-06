2e7a4e8f9a0547b71049c8d6b2aeb7b4d2190af4
meteor <hslxy417@foxmail.com>
2020-12-06 19:58:24 +0800

    demo2

---
+++ demo2/app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+
+
+const app = express()
+const port = 80
+
+
+
+
+
+// 引入ejs
+app.set('view engine', "ejs")
+
+
+
+
+//应用级中间件
+app.use((req, res, next) => {
+    console.log(new Date())
+    next()
+})
+//内置中间件
+app.use(express.static("static"))
+
+app.get('/', (req, res) => {
+    let title = "你好ejs";
+    res.render("index.ejs", {
+        title
+    })
+}
+)
+
+app.get('/news', (req, res) => {
+    let title = {
+        name: "张三",
+        age: 20
+    };
+    let aa = "<h2>aaaa</h2>"
+    res.render("index2", {
+        title: title,
+        aa: aa
+    })
+})
+
+app.get('/news/:id', (req, res) => {
+    res.send("news id")
+}
+)
+
+//错误处理中间件
+app.use((req, res, next) => {
+    res.status(404).send("404")
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
